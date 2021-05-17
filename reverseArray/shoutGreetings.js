0468aed6159976d2b9dd0c9f40803943cd36cb5e
falsaya <falsaya@ncsu.edu>
2021-05-17 18:43:19 -0400

    added another map method

---
+++ reverseArray/shoutGreetings.js
@@ -0,0 +1,8 @@
+const shoutGreetings = (array) => {
+  return array.map((ele) => `${ele.toUpperCase()}!`);
+};
+
+const greetings = ['hello', 'hi', 'heya', 'oi', 'hey', 'yo'];
+
+console.log(shoutGreetings(greetings))
+// Should print [ 'HELLO!', 'HI!', 'HEYA!', 'OI!', 'HEY!', 'YO!' ]
