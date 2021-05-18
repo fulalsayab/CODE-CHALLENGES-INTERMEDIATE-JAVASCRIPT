cc6cdc845e28921ad6a79d087275fb6d47aedafa
falsaya <falsaya@ncsu.edu>
2021-05-17 20:51:35 -0400

    indexOf method

---
+++ reverseArray/findMyKeys.js
@@ -0,0 +1,14 @@
+const findMyKeys = (array) => {
+  return array.indexOf("keys");
+};
+const randomStuff = [
+  "credit card",
+  "screwdriver",
+  "receipt",
+  "gum",
+  "keys",
+  "used gum",
+  "plastic spoon",
+];
+
+console.log(findMyKeys(randomStuff));
